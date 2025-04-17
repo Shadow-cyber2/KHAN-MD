25a5e9b81b1c5c617a207bad527bd73a7f3cba5f
Shadow-cyber2 <deonekevin3@gmail.com>
2025-04-17 13:43:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "?",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
 // add bot namw here for menu
